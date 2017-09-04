8e946a972498f7f0a87cfb53785f9fb41cd9edea
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-09-04 17:29:37 -0300

    Wire up react and redux

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Home from './Home';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<BrowserRouter>
+					<div>
+						<Route path="/home" component={Home} />
+					</div>
+				</BrowserRouter>
+			</div>
+		);
+	}
+}
+
+export default App;
